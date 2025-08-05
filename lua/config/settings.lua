447356d7e794c671056272ade39dcd5edade927f
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-08-05 17:51:25 -0300

    Lsp e completion + configs

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,7 +1,58 @@
-vim.o.number = true
-vim.o.relativenumber = true
+-- [[ Setting options ]]
 
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- Set the line number and make it relative
+vim.wo.cursorcolumn = true
+vim.wo.cursorline = true
+vim.wo.so = 15
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.scrolloff = 999
+
+vim.o.hlsearch = false
+vim.o.mouse = "a"
+vim.o.breakindent = true
+vim.o.undofile = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = "unnamedplus"
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = "menuone,noselect"
+
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.o.confirm = true
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
