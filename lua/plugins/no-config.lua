7d61bfc2548f51319c7cfc83dd18464e84b0e098
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-08-05 20:02:01 -0300

    autoclose, colorscheme e remocao de comentarios

--- lua/plugins/no-config.lua
+++ lua/plugins/no-config.lua
@@ -1,5 +1,10 @@
-return{
-    { "nvim-tree/nvim-web-devicons", opts = {} },
-    { "mason-org/mason.nvim", opts = {} },
-    { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = { icons_enabled = true, theme = "material" } } 
+return {
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{ "mason-org/mason.nvim", opts = {} },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = { icons_enabled = true, theme = "material" },
+	},
+	{ "m4xshen/autoclose.nvim", opts = {} },
 }
