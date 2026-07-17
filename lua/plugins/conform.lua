6b68b39474ce55adc85d5da444c7a2fef169a6ff
Diogo Matos <alvesdiogo.mts@gmail.com>
2026-07-17 14:09:42 -0300

    conform formatter de html com djlint

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,24 @@
-vim.pack.add { "https://github.com/stevearc/conform.nvim" }
+vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
 
 require("conform").setup({
-    default_format_opts = {
-        lsp_format = 'fallback',
-    },
-    formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        javascript = { "prettier", stop_after_first = true },
-        html = { "djlint", "prettier" },
-        jinja = { "djlint" },
-        htmldjango = { "djlint" },
-    },
-    format_on_save = {
-        lsp_format = "fallback",
-        timeout_ms = 500,
-    },
+	default_format_opts = {
+		lsp_format = "fallback",
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { "prettier", stop_after_first = true },
+		html = { "djlint" },
+		jinja = { "djlint" },
+		htmldjango = { "djlint" },
+	},
+	format_on_save = {
+		lsp_format = "fallback",
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
 
-vim.keymap.set("n", "<Leader>ff", function() require('conform').format({ async = true, lsp_fallback = true }) end)
+vim.keymap.set("n", "<Leader>ff", function()
+	require("conform").format({ async = true, lsp_fallback = true })
+end)
